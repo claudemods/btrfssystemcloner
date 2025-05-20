bf13fcdf53627f4e22d5f709eea28747310f43b0
claudemods <73653396+claudemods@users.noreply.github.com>
2025-05-20 19:30:55 +0100

    .sh for handheld editions

---
+++ onetime-launcherhandheld.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Clone the repo into projects directory
+git clone https://github.com/claudemods/btrfssystemcloner.git
+
+# Copy the whole damn folder to /opt
+cp -r btrfssystemcloner/place-in-opt/btrfssystemclonercachyoshandheld /opt
+
+# Delete the original clone
+rm -rf btrfssystemcloner
+
+chmod +x /opt/btrfssystemcloner/btrfssystemclonercachyoshandheld
+
+# Run the fucking command
+cd /opt/btrfssystemcloner && ./btrfssystemclonercachyoshandheld
