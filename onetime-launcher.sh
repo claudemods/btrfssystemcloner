69012172a205e862b69217c6ab95ecdc3e30a15d
claudemods <73653396+claudemods@users.noreply.github.com>
2025-05-23 13:20:38 +0100

    Update onetime-launcher.sh

--- onetime-launcher.sh
+++ onetime-launcher.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
+# Function to run commands with cyan color
+cyan_run() {
+    echo -ne "\033[38;2;0;255;255m"  # Set cyan color
+    "$@"
+    echo -ne "\033[0m"  # Reset color
+}
+
 # Clone the repo into projects directory
-git clone https://github.com/claudemods/btrfssystemcloner.git
+cyan_run git clone https://github.com/claudemods/btrfssystemcloner.git
 
 # set opt directory to be user owned
+cyan_run sudo chown $USER /opt
 
-sudo chown $USER /opt
+# make the needed folders
+cyan_run mkdir /opt/Arch-Systemtool
 
-# make the needed folers 
+# setup application and place .desktop icon etc.....
+cyan_run cp -r btrfssystemcloner/place-in-opt/btrfssystemcloner /opt
+cyan_run cp -r btrfssystemcloner/onetime-launcher.sh /opt/Arch-Systemtool
+cyan_run sudo cp -r btrfssystemcloner/btrfssystemcloner.desktop /usr/share/applications
+cyan_run cp -r btrfssystemcloner/btrfssystemcloner.png /opt/Arch-Systemtool
 
-mkdir /opt/Arch-Systemtool
 
-# setup application and place .desktop icon ect.....
-cp -r btrfssystemcloner/place-in-opt/btrfssystemcloner /opt
+cyan_run chmod +x /opt/btrfssystemcloner/btrfssystemclonercachyos
+cyan_run chmod +x /usr/share/applications/btrfssystemcloner.desktop
 
-cp -r onetime-launcher.sh /opt/Arch-Systemtool
 
-cp -r btrfssystemcloner.desktop /usr/share/applications
+cyan_run rm -rf btrfssystemcloner
 
-cp -r btrfssystemcloner.png /opt/Arch-Systemtool
-
-# Delete the original clone
-rm -rf btrfssystemcloner
-
-chmod +x /opt/btrfssystemcloner/btrfssystemclonercachyos
-
-chmod +x /usr/share/applications/btrfssystemcloner.desktop
-
-# Run the fucking command
+# Run the command
+echo -ne "\033[38;2;0;255;255m"
 cd /opt/btrfssystemcloner && ./btrfssystemclonercachyos
+echo -ne "\033[0m"
