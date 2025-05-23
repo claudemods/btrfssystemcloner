934135514ee571f1b1707c0d73411119dea0afc0
claudemods <73653396+claudemods@users.noreply.github.com>
2025-05-23 13:25:37 +0100

    Update onetime-launcherhandheld.sh

--- onetime-launcherhandheld.sh
+++ onetime-launcherhandheld.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
+# Function to run commands with cyan color
+cyan_run() {
+    echo -ne "\033[38;2;0;255;255m"  # Set cyan color
+    "$@"
+    echo -ne "\033[0m"  # Reset color
+}
+
 # Clone the repo into projects directory
-git clone https://github.com/claudemods/btrfssystemcloner.git
+cyan_run git clone https://github.com/claudemods/btrfssystemcloner.git
 
-# Copy the whole damn folder to /opt
-cp -r btrfssystemcloner/place-in-opt/btrfssystemclonercachyoshandheld
+# set opt directory to be user owned
+cyan_run sudo chown $USER /opt
 
-# Delete the original clone
-rm -rf btrfssystemcloner
+# make the needed folders
+cyan_run mkdir /opt/Arch-Systemtool
 
-chmod +x /opt/btrfssystemcloner/btrfssystemclonercachyoshandheld
+# setup application and place .desktop icon etc.....
+cyan_run cp -r btrfssystemcloner/place-in-opt/btrfssystemcloner /opt
+cyan_run cp -r btrfssystemcloner/onetime-launcherhandheld.sh /opt/Arch-Systemtool
+cyan_run sudo cp -r btrfssystemcloner/btrfssystemclonerhandheld.desktop /usr/share/applications
+cyan_run cp -r btrfssystemcloner/btrfssystemcloner.png /opt/Arch-Systemtool
 
-# Run the fucking command
+
+cyan_run chmod +x /opt/btrfssystemcloner/btrfssystemclonercachyoshandheld
+cyan_run chmod +x /usr/share/applications/btrfssystemclonerhandheld.desktop
+
+
+cyan_run rm -rf btrfssystemcloner
+
+# Run the command
+echo -ne "\033[38;2;0;255;255m"
 cd /opt/btrfssystemcloner && ./btrfssystemclonercachyoshandheld
+echo -ne "\033[0m"
