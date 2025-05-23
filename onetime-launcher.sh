b4449a1e59eaccc00cd4bf276aa8cc39899a8e21
claudemods <73653396+claudemods@users.noreply.github.com>
2025-05-23 13:10:56 +0100

    Update onetime-launcher.sh

--- onetime-launcher.sh
+++ onetime-launcher.sh
@@ -3,13 +3,29 @@
 # Clone the repo into projects directory
 git clone https://github.com/claudemods/btrfssystemcloner.git
 
-# Copy the whole damn folder to /opt
+# set opt directory to be user owned
+
+sudo chown $USER /opt
+
+# make the needed folers 
+
+mkdir /opt/Arch-Systemtool
+
+# setup application and place .desktop icon ect.....
 cp -r btrfssystemcloner/place-in-opt/btrfssystemcloner /opt
+
+cp -r onetime-launcher.sh /opt/Arch-Systemtool
+
+cp -r btrfssystemcloner.desktop /usr/share/applications
+
+cp -r btrfssystemcloner.png 
 
 # Delete the original clone
 rm -rf btrfssystemcloner
 
 chmod +x /opt/btrfssystemcloner/btrfssystemclonercachyos
+
+chmod +x /usr/share/applications/btrfssystemcloner.desktop
 
 # Run the fucking command
 cd /opt/btrfssystemcloner && ./btrfssystemclonercachyos
