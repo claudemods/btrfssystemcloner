5c811a4b08651e5157116af91b8ec53f93b2c29e
claudemods <73653396+claudemods@users.noreply.github.com>
2025-05-31 00:45:21 +0100

    Update onetime-launcher.sh

--- onetime-launcher.sh
+++ onetime-launcher.sh
@@ -23,6 +23,7 @@ cyan_run sudo cp -r btrfssystemcloner/btrfssystemcloner.png /opt/Arch-Systemtool
 
 cyan_run sudo chmod +x /opt/btrfssystemcloner/btrfssystemclonercachyos
 cyan_run sudo chmod +x /usr/share/applications/btrfssystemcloner.desktop
+cyan_run sudo chmod +x /opt/Arch-Systemtool/btrfssystemcloner/onetime-launcher.sh
 
 
 cyan_run rm -rf btrfssystemcloner
