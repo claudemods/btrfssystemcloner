4e2dab82497a4077348ffc85dc83550c0e640e17
claudemods <73653396+claudemods@users.noreply.github.com>
2025-06-03 13:55:21 -0700

    Update onetime-launcher.sh

--- onetime-launcher.sh
+++ onetime-launcher.sh
@@ -12,18 +12,18 @@ cyan_run git clone https://github.com/claudemods/btrfssystemcloner.git
 
 
 # make the needed folders
-cyan_run mkdir /opt/Arch-Systemtool
+cyan_run sudo mkdir /opt/Arch-Systemtool
 
 # setup application and place .desktop icon etc.....
 cyan_run sudo cp -r btrfssystemcloner/place-in-opt/btrfssystemcloner /opt
-cyan_run sudo cp -r btrfssystemcloner/onetime-launcher.sh /opt/Arch-Systemtool
+cyan_run sudo cp -r btrfssystemcloner/onetime-launcher.sh /opt/Arch-Systemtool/onetime-launcher.sh
 cyan_run sudo cp -r btrfssystemcloner/btrfssystemcloner.desktop /usr/share/applications
-cyan_run sudo cp -r btrfssystemcloner/btrfssystemcloner.png /opt/Arch-Systemtool
+cyan_run sudo cp -r btrfssystemcloner/btrfssystemcloner.png /opt/Arch-Systemtool/btrfssystemcloner.png
 
 
 cyan_run sudo chmod +x /opt/btrfssystemcloner/btrfssystemclonercachyos
 cyan_run sudo chmod +x /usr/share/applications/btrfssystemcloner.desktop
-cyan_run sudo chmod +x /opt/Arch-Systemtool/btrfssystemcloner/onetime-launcher.sh
+cyan_run sudo chmod +x /opt/Arch-Systemtool/onetime-launcher.sh
 
 
 cyan_run rm -rf btrfssystemcloner
