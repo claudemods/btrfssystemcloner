fd668ab205b371aa48daf0877342ebb68028033f
claudemods <73653396+claudemods@users.noreply.github.com>
2025-06-03 13:54:27 -0700

    Update onetime-launcherhandheld.sh

--- onetime-launcherhandheld.sh
+++ onetime-launcherhandheld.sh
@@ -12,18 +12,18 @@ cyan_run git clone https://github.com/claudemods/btrfssystemcloner.git
 
 
 # make the needed folders
-cyan_run mkdir /opt/Arch-Systemtool
+cyan_run sudo mkdir /opt/Arch-Systemtool
 
 # setup application and place .desktop icon etc.....
 cyan_run sudo cp -r btrfssystemcloner/place-in-opt/btrfssystemcloner /opt
-cyan_run sudo cp -r btrfssystemcloner/onetime-launcherhandheld.sh /opt/Arch-Systemtool
+cyan_run sudo cp -r btrfssystemcloner/onetime-launcherhandheld.sh /opt/Arch-Systemtool/onetime-launcherhandheld.sh
 cyan_run sudo cp -r btrfssystemcloner/btrfssystemclonerhandheld.desktop /usr/share/applications
-cyan_run sudo cp -r btrfssystemcloner/btrfssystemcloner.png /opt/Arch-Systemtool
+cyan_run sudo cp -r btrfssystemcloner/btrfssystemcloner.png /opt/Arch-Systemtool/btrfssystemcloner.png
 
 
 cyan_run sudo chmod +x /opt/btrfssystemcloner/btrfssystemclonercachyoshandheld
 cyan_run sudo chmod +x /usr/share/applications/btrfssystemclonerhandheld.desktop
-cyan_run sudo chmod +x /opt/Arch-Systemtool/btrfssystemcloner/onetime-launcherhandheld.sh
+cyan_run sudo chmod +x /opt/Arch-Systemtool/onetime-launcherhandheld.sh
 
 
 cyan_run rm -rf btrfssystemcloner
