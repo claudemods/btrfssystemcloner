780c7c221c01bb5313d5082ac517d954046a3649
claudemods <73653396+claudemods@users.noreply.github.com>
2025-07-18 17:16:24 +0100

    new fixes testing now

--- testing scripts/imgoverlaysystem.sh
+++ testing scripts/imgoverlaysystem.sh
@@ -21,6 +21,18 @@ sudo mount -o subvol=@ "${LOOP_DEV}p2" /mnt/primary || {
     exit 1
 }
 
+sudo mount -o subvol=@home "${LOOP_DEV}p2" /mnt/primary || {
+    echo "ERROR: Failed to mount @ on ${LOOP_DEV}p2" >&2
+    sudo losetup -d "$LOOP_DEV"
+    exit 1
+}
+
+sudo mount -o subvol=@root "${LOOP_DEV}p2" /mnt/primary || {
+    echo "ERROR: Failed to mount @ on ${LOOP_DEV}p2" >&2
+    sudo losetup -d "$LOOP_DEV"
+    exit 1
+}
+
 sudo mount "${LOOP_DEV}p1" /mnt/primary/boot/efi || {
     echo "ERROR: EFI mount failed on ${LOOP_DEV}p1" >&2
     sudo umount /mnt/primary
@@ -29,11 +41,11 @@ sudo mount "${LOOP_DEV}p1" /mnt/primary/boot/efi || {
 }
 
 # Bind mounts (ALL of them)
-sudo mount --bind /dev /mnt/dev
-sudo mount --bind /proc /mnt/proc
-sudo mount --bind /sys /mnt/sys
-sudo mount --bind /run /mnt/run
-sudo mount --bind /dev/pts /mnt/dev/pts
+sudo mount --bind /dev /mnt/primary/dev
+sudo mount --bind /proc /mnt/primary/proc
+sudo mount --bind /sys /mnt/primary/sys
+sudo mount --bind /run /mnt/primary/run
+sudo mount --bind /dev/pts /mnt/primary/dev/pts
 
 # Create a symlink in your home directory instead of root
 ln -s /mnt/primary /
